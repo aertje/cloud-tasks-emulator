0a2c4af9a75568a33183197db76d8388bf16ac71
op57no23 <grant.audet@gmail.com>
2023-02-13 14:05:04 -0800

    read from env vars

--- emulator.go
+++ emulator.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strings"
 	"sync"
+	"os"
 
 	tasks "google.golang.org/genproto/googleapis/cloud/tasks/v2"
 	v1 "google.golang.org/genproto/googleapis/iam/v1"
@@ -329,18 +330,6 @@ func (s *Server) RunTask(ctx context.Context, in *tasks.RunTaskRequest) (*tasks.
 	return taskState, nil
 }
 
-// arrayFlags used for parsing list of potentially repeated flags e.g. -queue $Q1 -queue $Q2
-type arrayFlags []string
-
-func (i *arrayFlags) String() string {
-	return strings.Join(*i, ", ")
-}
-
-func (i *arrayFlags) Set(value string) error {
-	*i = append(*i, value)
-	return nil
-}
-
 // Creates an initial queue on the emulator
 func createInitialQueue(emulatorServer *Server, name string) {
 	print(fmt.Sprintf("Creating initial queue %s\n", name))
@@ -361,14 +350,8 @@ func createInitialQueue(emulatorServer *Server, name string) {
 }
 
 func main() {
-	var initialQueues arrayFlags
-
-	host := flag.String("host", "localhost", "The host name")
-	port := flag.String("port", "8123", "The port")
 	openidIssuer := flag.String("openid-issuer", "", "URL to serve the OpenID configuration on, if required")
 	hardResetOnPurgeQueue := flag.Bool("hard-reset-on-purge-queue", false, "Set to force the 'Purge Queue' call to perform a hard reset of all state (differs from production)")
-
-	flag.Var(&initialQueues, "queue", "A queue to create on startup (repeat as required)")
 
 	flag.Parse()
 
@@ -380,20 +363,24 @@ func main() {
 		defer srv.Shutdown(context.Background())
 	}
 
-	lis, err := net.Listen("tcp", fmt.Sprintf("%v:%v", *host, *port))
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("%v:%v", host, port))
 	if err != nil {
 		panic(err)
 	}
 
-	print(fmt.Sprintf("Starting cloud tasks emulator, listening on %v:%v\n", *host, *port))
+	print(fmt.Sprintf("Starting cloud tasks emulator, listening on %v:%v\n", host, port))
 
 	grpcServer := grpc.NewServer()
 	emulatorServer := NewServer()
 	emulatorServer.Options.HardResetOnPurgeQueue = *hardResetOnPurgeQueue
 	tasks.RegisterCloudTasksServer(grpcServer, emulatorServer)
 
-	for i := 0; i < len(initialQueues); i++ {
-		createInitialQueue(emulatorServer, initialQueues[i])
+	initialQueue := os.Getenv("INITIAL_QUEUE")
+	if initialQueue != "" {
+		createInitialQueue(emulatorServer, initialQueue)
 	}
 
 	grpcServer.Serve(lis)
